dbd7fee7eb2c1f7155b9bac49bb35f07b9a3084f
kle1603 <bkhang160303@gmail.com>
2023-12-25 23:56:18 +0700

    feat: add feature sign up

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,5 @@
+export { default as addDocument } from "./addDocument";
+export { default as setDocument } from "./setDocument";
 export { default as signInWithEmail } from "./signInWithEmail";
 export { default as signOutSystem } from "./signOutSystem";
 export { default as signUpWithEmail } from "./signUpWithEmail";
