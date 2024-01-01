8519ab4c18de7b10238eb8f9a444428414b74b95
kle1603 <bkhang160303@gmail.com>
2024-01-01 15:19:19 +0700

    style: page not found

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,27 @@
+import Button from "@/components/Button";
+import Logo from "@/components/Logo";
+import configs from "@/configs";
+
 const NotFound = () => {
-    return <div>NotFound</div>;
+    return (
+        <div className="not-found">
+            <Logo className="not-found__logo" />
+
+            <div className="not-found__content">
+                <div className="not-found__text">Page not found !</div>
+
+                <div className="not-found__number">404</div>
+
+                <Button
+                    variant="secondary"
+                    to={configs.routes.home}
+                    className="not-found__button"
+                >
+                    RETURN TO HOME
+                </Button>
+            </div>
+        </div>
+    );
 };
 
 export default NotFound;
