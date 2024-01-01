99d1761bd0803987957444f4d4531a1b81e4099d
kle1603 <bkhang160303@gmail.com>
2024-01-01 19:09:04 +0700

    style: change template page not found

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,25 +1,41 @@
-import Button from "@/components/Button";
-import Logo from "@/components/Logo";
+import { Link } from "react-router-dom";
+
 import configs from "@/configs";
 
 const NotFound = () => {
     return (
         <div className="not-found">
-            <Logo className="not-found__logo" />
-
-            <div className="not-found__content">
-                <div className="not-found__text">Page not found !</div>
-
-                <div className="not-found__number">404</div>
-
-                <Button
-                    variant="secondary"
-                    to={configs.routes.home}
-                    className="not-found__button"
-                >
-                    RETURN TO HOME
-                </Button>
+            <div id="particles" className="not-found__particles">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
+
+            <section className="not-found__body">
+                <h1 className="not-found__title">Page Not Found!</h1>
+                <div className="not-found__number">
+                    <span>4</span>
+                    <span className="not-found__circle">0</span>
+                    <span>4</span>
+                </div>
+                <p className="not-found__text">
+                    We are unable to find the page
+                </p>
+                <p className="not-found__text">you're looking for</p>
+
+                <Link to={configs.routes.home} className="not-found__button">
+                    Back to Home Page
+                </Link>
+            </section>
         </div>
     );
 };
