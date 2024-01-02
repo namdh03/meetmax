bd2028e107fd349bad5fb12984be62f67538e519
kle1603 <bkhang160303@gmail.com>
2024-01-02 10:03:38 +0700

    fix: change rem to px and create color variable

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,40 +1,42 @@
-import { Link } from "react-router-dom";
-
+import Button from "@/components/Button";
 import configs from "@/configs";
 
 const NotFound = () => {
     return (
         <div className="not-found">
             <div id="particles" className="not-found__particles">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
+                <span className="not-found__star"></span>
+                <span className="not-found__star"></span>
+                <span className="not-found__star"></span>
+                <span className="not-found__star"></span>
+                <span className="not-found__star"></span>
+                <span className="not-found__star"></span>
+                <span className="not-found__star"></span>
+                <span className="not-found__star"></span>
+                <span className="not-found__star"></span>
+                <span className="not-found__star"></span>
+                <span className="not-found__star"></span>
+                <span className="not-found__star"></span>
             </div>
 
             <section className="not-found__body">
                 <h1 className="not-found__title">Page Not Found!</h1>
                 <div className="not-found__number">
-                    <span>4</span>
+                    <span className="not-found__span">4</span>
                     <span className="not-found__circle">0</span>
-                    <span>4</span>
+                    <span className="not-found__span">4</span>
                 </div>
                 <p className="not-found__text">
-                    We are unable to find the page
+                    We are unable to find the page you're looking for.
                 </p>
-                <p className="not-found__text">you're looking for</p>
 
-                <Link to={configs.routes.home} className="not-found__button">
+                <Button
+                    variant="secondary"
+                    to={configs.routes.home}
+                    className="not-found__button"
+                >
                     Back to Home Page
-                </Link>
+                </Button>
             </section>
         </div>
     );
