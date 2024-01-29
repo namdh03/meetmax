9bafde0d01fe44defa05a82d56f428c5144b72ee
namdh03 <duonghoangnam503@gmail.com>
2024-01-29 14:34:25 +0700

    refactor: change site bar ui

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -13,39 +13,9 @@ const MainLayout = () => {
 
     const siteBarList: SiteBarItemType[] = [
         {
-            to: configs.routes.feed,
-            label: "Feed",
-            icon: icons.feed,
-            count: 0,
-        },
-        {
-            to: configs.routes.community,
-            label: "My community",
-            icon: icons.community,
-            count: 0,
-        },
-        {
             to: configs.routes.messages,
             label: "Messages",
             icon: icons.message,
-            count: 0,
-        },
-        {
-            to: configs.routes.notification,
-            label: "Notification",
-            icon: icons.notification,
-            count: 0,
-        },
-        {
-            to: configs.routes.explore,
-            label: "Explore",
-            icon: icons.explore,
-            count: 0,
-        },
-        {
-            to: configs.routes.profile,
-            label: "Profile",
-            icon: icons.user,
             count: 0,
         },
         {
